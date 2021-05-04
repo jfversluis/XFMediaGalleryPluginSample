23853518d3e5cd4f3f962c85600238c7139f19b2
Gerald Versluis <gerald@verslu.is>
2021-05-04 11:05:08 +0200

    Implement MediaGallery

--- XFMediaGalleryPluginSample.Android/MainActivity.cs
+++ XFMediaGalleryPluginSample.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Content;
 
 namespace XFMediaGalleryPluginSample.Droid
 {
@@ -13,11 +14,23 @@ namespace XFMediaGalleryPluginSample.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            NativeMedia.Platform.Init(this, savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if(NativeMedia.Platform.CheckCanProcessResult(requestCode, resultCode, data))
+            {
+                NativeMedia.Platform.OnActivityResult(requestCode, resultCode, data);
+            }
+
+            base.OnActivityResult(requestCode, resultCode, data);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- XFMediaGalleryPluginSample/MainPage.xaml.cs
+++ XFMediaGalleryPluginSample/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NativeMedia;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace XFMediaGalleryPluginSample
@@ -13,6 +15,32 @@ namespace XFMediaGalleryPluginSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void OnPickImagesClick(object sender, EventArgs args)
+        {
+            var results = await MediaGallery.PickAsync(3, MediaFileType.Image, MediaFileType.Video);
+
+            if (results?.Files == null)
+            {
+                return;
+            }
+
+            foreach (var media in results.Files)
+            {
+                var fileName = media.NameWithoutExtension;
+                var extension = media.Extension;
+                var contentType = media.ContentType;
+
+                await DisplayAlert(fileName, $"Extension: {extension}, Content-type: {contentType}", "OK");
+            }
+        }
+
+        private async void OnSaveImageClick(object sender, EventArgs args)
+        {
+            var screenshot = await Screenshot.CaptureAsync();
+
+            await MediaGallery.SaveAsync(MediaFileType.Image, await screenshot.OpenReadAsync(), "myScreenshot.png");
         }
     }
 }
